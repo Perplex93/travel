8c941231eca2bcf9f98b9a68e030e501e7ac0db2
Jenny <jennifer.zarazik.93@gmail.com>
2020-12-11 20:25:43 +0100

    models and views

---
+++ app/Models/Category_Destination.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category_Destination extends Model
+{
+    protected $table = 'category_destination';
+}
---
+++ app/Models/Destination_Description.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Destination_Description extends Model
+{
+    protected $table = 'destination_description';
+
+}
